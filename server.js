85e31c9a76061f3c8f3865a90eebcd6558c86b1f
lucyprototip-dot <lucyprototip@gmail.com>
2026-06-07 17:54:08 +0300

    Delete server.js

--- server.js
+++
@@ -1,5 +0,0 @@
-// Legacy entry point guard.
-// Asıl LUCY backend uygulaması system.js içindedir.
-// Deploy yanlışlıkla `node server.js` çalıştırsa bile toolOrchestrator devre dışı kalmasın.
-console.warn("[LUCY] server.js legacy wrapper: system.js başlatılıyor...");
-require("./system.js");
